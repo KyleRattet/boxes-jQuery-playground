3da5dc778b5b308fe2a041f6872d49c95e3772bf
KyleRattet <krattet@gmail.com>
2015-07-28 14:11:01 -0600

    exercise 2 complete

--- boxes.js
+++ boxes.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
 
-    $('#secretBox').css('background-color','white');
+    $('#secretBox').css({
+      'background-color': 'white',
+      "color" : "black"
+    });
+
     $('#secretBox').append('<h1>secret box!</h1>');
+
+    $('#row1').children().removeClass().attr('class','box boxType3');
+
+    $('#row4').children().last().hide();
+
+    $('.boxType1').css("background-color", "white");
+
+    $('#row2 div:lt(2)').css('visibility', 'hidden');
+
+    $('.box').not("#secretBox").css('width', '2px');
 });
