fc42844ef70387f9c0f007a18fee888c75947d5d
KyleRattet <krattet@gmail.com>
2015-07-29 16:02:54 -0600

    updated exercise 3 part 4

--- boxes.js
+++ boxes.js
@@ -5,18 +5,19 @@ $( document ).ready(function() {
       "color" : "black"
     });
 
-    // $('#secretBox').append('<h1>secret box!</h1>');
+    $('#secretBox').append('<h1>secret box!</h1>');
 
-    // $('#row1').children().removeClass().attr('class','box boxType3');
+    $('#row1').children().removeClass().attr('class','box boxType3');
 
-    // $('#row4').children().last().hide();
+    $('#row4').children().last().hide();
 
-    // $('.boxType1').css("background-color", "white");
+    $('.boxType1').css("background-color", "white");
 
-    // $('#row2 div:lt(2)').css('visibility', 'hidden');
+    $('#row2 div:lt(2)').css('visibility', 'hidden');
 
-    // $('.box').not("#secretBox").css('width', '2px');
-   //exercise 3
+    $('.box').not("#secretBox").css('width', '2px');
+
+   // exercise 3
     $('#container').on('click', function  () {
        console.log(event.pageX, event.pageY);
     });
@@ -26,31 +27,14 @@ $( document ).ready(function() {
         alert("You can never leave!");
     });
 
-    // $('.box').on('click', function () {
-    //   $(this).append('<img src="https://socialsteve.files.wordpress.com/2012/07/social-media-puppy.jpg">').on('click', function  () {
-    //     $('img').hide();
-    //   });
-    // });
-
-    // $('.box').on('click', function() {
-    //   $(this).css('background', 'white')
-    //   $('#container').css('background', 'black')
-    //     $('#container').on('click', function  () {
-    //       $('#container').css('background', 'limegreen')
-    //     });
-    // });
-
-    $('#container').on('click', function () {
-      if (clicked container but not box) {
-        $(this).css('background', 'lime')
-        //code
-      } else {
-    })
-
-    $('.box').on('click', function () {
-      $(this).css('background', 'white')
-      $('#container').css("background", 'black')
-    })
-
+    $('#container').on('click', function (e) {
+      if ($(e.target).hasClass('box') === true) {
+        $(e.target).css('background', 'white');
+        $('#container').css('background', 'black');
+      }
+      else if ($(this).is('#container') === true) {
+        $(this).css("background", "limegreen");
+    }
+  });
 
 });
